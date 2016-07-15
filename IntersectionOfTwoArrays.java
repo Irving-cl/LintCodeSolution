0fe75a538d7f2eccec636a4f1e13a7186ba455a6
Irving-cl <oos1111@sjtu.edu.cn>
2016-07-15 23:34:22 +0800

    add 3 solutions

---
+++ IntersectionOfTwoArrays.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class IntersectionOfTwoArrays {
+
+    /**
+     * @param nums1 an integer array
+     * @param nums2 an integer array
+     * @return an integer array
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums1.length; i++) {
+            if (!set.contains(nums1[i])) {
+                set.add(nums1[i]);
+            }
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                list.add(nums2[i]);
+                set.remove(nums2[i]);
+            }
+        }
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+}

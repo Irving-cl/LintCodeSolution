23b85fd796f399d86f320e5aed6e4773338d1f21
Irving-cl <oos1111@sjtu.edu.cn>
2016-08-06 00:44:03 +0800

    add 2 solutions

---
+++ UglyNumber.java
@@ -0,0 +1,27 @@
+
+public class UglyNumber {
+
+    /**
+     * @param num an integer
+     * @return true if num is an ugly number or false
+     */
+    public boolean isUgly(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        return num == 1;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}

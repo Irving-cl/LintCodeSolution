8daa585b4e56535bd90af017828fec5b55846506
Irving-cl <oos1111@sjtu.edu.cn>
2016-08-14 10:07:25 +0800

    add 2 solutions

---
+++ Search2DMatrix.java
@@ -0,0 +1,33 @@
+
+public class Search2DMatrix {
+
+    /**
+     * @param matrix, a list of lists of integers
+     * @param target, an integer
+     * @return a boolean, indicate whether matrix contains target
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null) {
+            return false;
+        }
+        int m = matrix.length;
+        if (m == 0) {
+            return false;
+        }
+        int n = matrix[0].length;
+        for (int i = m - 1, j = 0; j < n && i >= 0; ) {
+            if (target == matrix[i][j]) {
+                return true;
+            } else if (target > matrix[i][j]) {
+                j++;
+            } else {
+                i--;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+};

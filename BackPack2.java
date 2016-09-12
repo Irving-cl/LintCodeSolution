52b2a9f66e924c124c115b95b5dea4e20c7c0d83
Irving-cl <oos1111@sjtu.edu.cn>
2016-09-12 10:59:09 +0800

    add 2 solutions

--- BackPack2.java
+++ BackPack2.java
@@ -1,5 +1,5 @@
 
-public class BackPack2{
+public class BackPack2 {
     /**
      * @param m: An integer m denotes the size of a backpack
      * @param A & V: Given n items with size A[i] and value V[i]
